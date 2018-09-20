34348a98146e0fce2340324926a45bce8dcc7be7
Codepuree <Codepuree@bitbucket.org>
2018-09-20 12:15:23 +0200

    add: post capability to api

--- api.js
+++ api.js
@@ -1,9 +1,10 @@
 class API {
     constructor() {
-        this.ipv6 = '2001:4ca0:0:f27f:e803:d37b:cd47:3015'
+        // this.ip = '[2001:4ca0:0:f27f:e803:d37b:cd47:3015]'
+        this.ip = 'app.jaka.xyz'
         this.port = 8088
         this.version = 'v1'
-        this.baseUrl = `http://[${this.ipv6}]:${this.port}/`
+        this.baseUrl = `http://${this.ip}:${this.port}/`
         this.apiUrl = `${this.baseUrl}${this.version}/`
     }
 
@@ -72,6 +73,9 @@ class API {
 
         return fetch(`${url}?token=${this.accessToken}`, {
             method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
             body: JSON.stringify({
                 username: this.username, gender, motto, university, token: this.accessToken
             })
@@ -80,6 +84,54 @@ class API {
             .catch(error => {
                 console.error('Users::ERROR:', error)
                 this.onInfoMessage('<b>UsersProfile::ERROR:</b> ' + error.toString(), 'error')
+            })
+    }
+
+    /*
+        content: {
+            text: req.body.content.text,
+            metadata: {
+                date: req.body.content.metadata.date,
+                image: req.body.content.metadata.image,
+                geolocation: {
+                    lat: req.body.content.metadata.geolocation.lat,
+                    lon: req.body.content.metadata.geolocation.lon
+                }
+            }
+        },
+     */
+    post(content) {
+        const urlPath = `posts`
+        const url = this.apiUrl + urlPath
+
+        return fetch(`${url}?token=${this.accessToken}`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({
+                username: this.username,
+                content
+            })
+        })
+            .then(res => res.json())
+            .catch(error => {
+                console.error('Post::ERROR:', error)
+                this.onInfoMessage('<b>Post::ERROR:</b> ' + error.toString(), 'error')
+            })
+    }
+
+    getPosts() {
+        const urlPath = `posts`
+        const url = this.apiUrl + urlPath
+
+        return fetch(`${url}?token=${this.accessToken}`, {
+            method: 'GET'
+        })
+            .then(res => res.json())
+            .catch(error => {
+                console.error('Posts::ERROR:', error)
+                this.onInfoMessage('<b>Posts::ERROR:</b> ' + error.toString(), 'error')
             })
     }
 }
